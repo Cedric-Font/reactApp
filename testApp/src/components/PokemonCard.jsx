99ed6c3979a9bd332431c75b3a1425c0bdd62077
Cedric-Font <font.cedric38@gmail.com>
2023-10-09 17:10:48 +0200

    pokemon dynamique

--- testApp/src/components/PokemonCard.jsx
+++ testApp/src/components/PokemonCard.jsx
@@ -1,11 +1,28 @@
 function PokemonCard(){
+    let pokemon = pokemonList[1]
 return (
 <div>
 <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-    <figcaption>Bulbasaur</figcaption>
+    {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p> }
+    <img src={pokemon.imgSrc} alt="" /> 
+    <figcaption>{pokemon.name}</figcaption>
     
     </figure>
-    </div>) ;}
+    </div>)
+     ;}
+
+    
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 
     export default PokemonCard; 
